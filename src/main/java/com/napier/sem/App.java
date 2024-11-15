a3bd5905406d84243554015579fd7df2c465ffe0
aktcope4066 <40663308@live.napier.ac.uk>
2024-11-15 14:50:26 +0000

    Updated the main method in the app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,17 @@ import java.sql.*;
 
 public class App
 {
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
     /**
      * Connection to MySQL database.
      */
