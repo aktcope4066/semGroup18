8249ea45b5d00f30439df6d5d0310a9fd650fdb4
ManikKhadiya <manikkhadiya@gmail.com>
2024-12-05 04:54:27 +0000

    Update capitalCity.java

--- src/main/java/com/napier/sem/classes/capitalCity.java
+++ src/main/java/com/napier/sem/classes/capitalCity.java
@@ -2,5 +2,32 @@ package com.napier.sem.classes;
 //class for capitakl city
 public class capitalCity {
     private String name;
-    private String Country;
+    private String country;
+    private int population;
+    //construct
+    public capitalCity(String name, String country, int population){
+        this.name = name;
+        this.country = country;
+        this.population = population;
+    };
+    //getters
+    public String getName() {
+        return name;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public int getPopulation() {
+        return population;
+    }
+    //setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setPopulation(int population){
+        this.population = population;
+    }
 }
