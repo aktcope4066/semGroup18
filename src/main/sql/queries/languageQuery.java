4cc4688b50409312dd06953acbc90a422b51ee6c
ManikKhadiya <manikkhadiya@gmail.com>
2024-12-05 04:16:22 +0000

    language query made

---
+++ src/main/sql/queries/languageQuery.java
@@ -0,0 +1,11 @@
+public class languageQuery {
+    //query string that will be used in database methods for language reports
+    public static String LanguageQuery = 
+        "SELECT countryLanguage.Language, " +
+        "   ROUND((SUM(countryLanguage.Percentage / 100 * country.Population) / (SELECT SUM(Population) FROM country)) * 100, 2)" +
+        "FROM countryLanguage " + 
+        "JOIN country ON countryLanguage.CountryCode = country.Code" +
+        "WHERE countryLanguage.Languages IN ('Chinese', 'Hindi', 'Spanish', 'English', 'Arabic') " + 
+        "GROUP BY countryLanguage.Language " +
+        "ORDER BY Population DESC";
+}
